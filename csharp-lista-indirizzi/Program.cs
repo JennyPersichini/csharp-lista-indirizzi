c925c851724d0d5e84e7099bd6da83fea9a1d428
JennyPersichini <nenny1096@gmail.com>
2023-04-26 15:41:03 +0200

    init program

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+
+using csharp_lista_indirizzi;
+
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+
+try
+{
+    StreamReader file = File.OpenText("C:\\Users\\valer\\OneDrive\\Desktop\\Generetion_Exercises\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\addresses.csv");
+    int rigacorrente = 0;
+
+
+    while (!file.EndOfStream)
+    {
+        rigacorrente++;
+        string line = file.ReadLine();
+
+        
+
+    }
+
+    file.Close();
+
+}
