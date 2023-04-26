de9a4a2ac528f51e4bc682b0f08f88b80f76242a
JennyPersichini <nenny1096@gmail.com>
2023-04-26 20:08:44 +0200

    add program

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -16,10 +16,62 @@ try
         rigacorrente++;
         string line = file.ReadLine();
 
-        
+        if (rigacorrente != 1)
+        {
+            Console.WriteLine(line);
+            string[] Listainformazioni = line.Split(',');
+            int numeroInformazioni = (Listainformazioni.Length);
+
+            if (numeroInformazioni < 6)
+            {
+                Console.WriteLine("L'indirizzo a riga " + rigacorrente + " non presenta tutte le informazioni necessarie né la giusta formattazione");
+                continue;
+            }
+            else if (numeroInformazioni > 6)
+            {
+
+                if (!char.IsDigit(Listainformazioni[2], 0))
+                {
+                    string[] nuovaListaInformazioni = new string[6];
+
+                    for (int i = 0; i < Listainformazioni.Length; i++)
+                    {
+                        if (i == 2)
+                        {
+                            nuovaListaInformazioni[1] += " " + Listainformazioni[2];
+                        }
+                        else if (i > 2)
+                        {
+                            nuovaListaInformazioni[i - 1] = Listainformazioni[i];
+                        }
+                        else
+                        {
+                            nuovaListaInformazioni[i] = Listainformazioni[i];
+                        }
+                    }
+                    Listainformazioni = nuovaListaInformazioni;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Listainformazioni.Length; i++)
+                {
+                    if (Listainformazioni[i] == null || Listainformazioni[i] == "")
+                    {
+                        Listainformazioni[i] = "undefined";
+                    }
+                }
+
+                Indirizzo indirizzo = new Indirizzo(Listainformazioni[0], Listainformazioni[1], Listainformazioni[2], Listainformazioni[3], Listainformazioni[4], Listainformazioni[5]);
+                listaIndirizzi.Add(indirizzo);
+            }
+        }
 
     }
 
     file.Close();
+
+} finally
+{
 
 }
